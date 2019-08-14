92a986cc38b4fe5851a19762e0a2193647cad1e1
Kimura0726 <0726.kimura@gmail.com>
2019-08-14 16:32:42 +0900

    [fix] #1 ヘッダー修正

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types';
 import Bar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import style from '../styles/AppBar.js'
 
 class AppBar extends Component {
@@ -16,13 +13,9 @@ class AppBar extends Component {
             <div className={classes.root}>
                 <Bar className={classes.Bar} position="static">
                     <Toolbar>
-                        <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
                         <Typography className={classes.title} variant="h6" >
-                            VSE
-                    </Typography>
-                        <Button className={classes.button} color="inherit">Login</Button>
+                            React Test
+                        </Typography>
                     </Toolbar>
                 </Bar>
             </div>
